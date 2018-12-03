158a19f39e39452adbe4fa77cb11f796e8f8d6ae
willf80 <willyfkouadio@gmail.com>
2018-12-03 09:02:40 +0000

    Function dateToStringFormat implemented

--- app/src/main/java/com/appinlab/moodtracker/utils/Constants.java
+++ app/src/main/java/com/appinlab/moodtracker/utils/Constants.java
@@ -1,5 +1,14 @@
 package com.appinlab.moodtracker.utils;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class Constants {
     public static final String TAG = "MoodTracker";
+
+    public static String dateToStringFormat(Date date, String pattern) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern, Locale.getDefault());
+        return simpleDateFormat.format(date);
+    }
 }
