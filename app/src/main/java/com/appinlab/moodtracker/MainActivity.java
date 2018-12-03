ab216d448f84bbf45e3222196341476b620b3035
willf80 <willyfkouadio@gmail.com>
2018-12-03 09:05:44 +0000

    Store the Mood comment, date, color, image et date of creation or update when user click on "Valider" button in CommentDialog

--- app/src/main/java/com/appinlab/moodtracker/MainActivity.java
+++ app/src/main/java/com/appinlab/moodtracker/MainActivity.java
@@ -10,11 +10,18 @@ import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.appinlab.moodtracker.models.Mood;
 import com.appinlab.moodtracker.models.MoodManager;
+import com.appinlab.moodtracker.models.MoodStore;
 import com.appinlab.moodtracker.utils.Constants;
 import com.appinlab.moodtracker.utils.OnSwipeTouchListener;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import io.realm.Realm;
 
 public class MainActivity extends AppCompatActivity implements CommentFragment.OnDispatcher {
 
@@ -22,12 +29,15 @@ public class MainActivity extends AppCompatActivity implements CommentFragment.O
     private ImageView mImageView;
 
     private MoodManager mMoodManager;
+    private Realm mRealm;
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRealm = Realm.getDefaultInstance();
 
         mMoodManager = new MoodManager();
 
@@ -80,5 +90,35 @@ public class MainActivity extends AppCompatActivity implements CommentFragment.O
     @Override
     public void onDoneClicked(String comment) {
         Log.d(Constants.TAG, "onDoneClicked: " + comment);
+
+        //Try to get today mood in database.
+        //If not exist, we create else we updated the last one
+        MoodStore moodStore = getTodayMood();
+        Mood mood = mMoodManager.getCurrentMood();
+
+        mRealm.beginTransaction();
+        if(moodStore == null) {
+            String id = Constants.dateToStringFormat(Calendar.getInstance().getTime(), "yyyyMMdd");
+
+            moodStore = new MoodStore();
+            moodStore.setId(id);
+        }
+
+        moodStore.setColor(mood.getColor());
+        moodStore.setImage(mood.getAvatar());
+        moodStore.setComment(comment);
+        moodStore.setDateAdd(Calendar.getInstance().getTime());
+        mRealm.commitTransaction();
+
+        //Date save with success Toast
+        Toast.makeText(this, "Commentaire enregisté avec succès !", Toast.LENGTH_LONG).show();
+    }
+
+    private MoodStore getTodayMood() {
+        Date date = Calendar.getInstance().getTime();
+        String id = Constants.dateToStringFormat(date, "yyyyMMdd");
+
+        return mRealm.where(MoodStore.class)
+                .equalTo("id", id).findFirst();
     }
 }
